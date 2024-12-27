e057dd97ed766a07c4030b76c84385799f948afe
Luka Strbad <s.luka.1308@gmail.com>
2024-12-27 17:26:08 +0100

    Changed @import to @use in scss

--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
-import { MdEditorComponent } from '../components/md-editor/md-editor.component';
 import { BrowserComponent } from "../browser/browser.component";
 
 @Component({
-    selector: 'app-home',
-    imports: [MdEditorComponent, BrowserComponent],
-    templateUrl: './home.component.html',
-    styleUrl: './home.component.scss'
+  selector: 'app-home',
+  imports: [BrowserComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   constructor(
