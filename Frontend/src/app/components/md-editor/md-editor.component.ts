6733c701130808a2590bf8cb6ce9e0687bca238d
Luka Strbad <s.luka.1308@gmail.com>
2025-01-19 16:17:46 +0100

    Replaced markdown-it with marked

--- Frontend/src/app/components/md-editor/md-editor.component.ts
+++ Frontend/src/app/components/md-editor/md-editor.component.ts
@@ -1,43 +1,110 @@
-import { Component, computed, effect, ElementRef, HostListener, model, Signal, signal, ViewChild, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  computed,
+  effect,
+  ElementRef,
+  HostListener,
+  model,
+  Signal,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import markdownit from 'markdown-it';
+import { Marked, TokensList } from 'marked';
+import { markedHighlight } from 'marked-highlight';
 import hljs from 'highlight.js';
-import MarkdownIt from 'markdown-it/index.js';
 import { SettingsService } from '../../../services/settings.service';
 import { EditorMode } from '../../../model/settings';
 import { TranslateDirective, TranslatePipe } from '@ngx-translate/core';
+import { EditorLineSize } from './editor-line.size';
+import { EditorIndex } from './editor.index';
 
 @Component({
   selector: 'app-md-editor',
   imports: [FormsModule, TranslateDirective, TranslatePipe],
   templateUrl: './md-editor.component.html',
   styleUrl: './md-editor.component.scss',
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class MdEditorComponent {
   value = model('');
 
   editorMode: Signal<EditorMode>;
-  showEditor = computed(() => this.editorMode() === "EditorOnly" || this.editorMode() === "SideBySide");
-  showPreview = computed(() => this.editorMode() === "PreviewOnly" || this.editorMode() === "SideBySide");
+  showEditor = computed(
+    () =>
+      this.editorMode() === 'EditorOnly' || this.editorMode() === 'SideBySide',
+  );
+  showPreview = computed(
+    () =>
+      this.editorMode() === 'PreviewOnly' || this.editorMode() === 'SideBySide',
+  );
 
   previewContent: string = '';
-  testDisplay = '';
 
-  private readonly md: MarkdownIt;
+  private readonly marked: Marked;
   // TODO: Use a better cache mechanism
   private highlightCache: Map<number, string> = new Map();
-  editorLines: EditorLine[] | null = null;
-  previewLines: PreviewLine[] | null = null;
+
+  editorLines: EditorLineSize[] | null = null;
+  editorIndices: EditorIndex[] = [];
+
+  @ViewChild('editorRef') editorElementRef!: ElementRef<HTMLTextAreaElement>;
   @ViewChild('previewRef') previewContentElement!: ElementRef<HTMLElement>;
   isMobile: boolean = false;
+  syncPreview = true;
 
-  constructor(
-    public settings: SettingsService
-  ) {
-    this.md = markdownit({
-      breaks: true,
-      highlight: (str, lang) => {
+  constructor(public settings: SettingsService) {
+    this.marked = new Marked(this.highlightExtension());
+
+    this.editorMode = computed(() => settings.mdEditor().editorMode);
+
+    effect(() => {
+      this.previewContent = this.marked.parse(this.value()) as string;
+      const tokens = this.marked.lexer(this.value());
+      this.editorIndices = this.calculateEditorIndex(tokens);
+    });
+
+    this.onWindowResize();
+  }
+
+  calculateEditorIndex(tokens: TokensList): EditorIndex[] {
+    const indices: EditorIndex[] = [];
+
+    let editorLineIndex = 0;
+    let previewElementIndex = 0;
+
+    for (const token of tokens) {
+      let editorLineCount = 0;
+      const raw: string | undefined = token['raw'];
+      if (raw) {
+        for (let i = 0; i < raw.length; i++) {
+          if (raw[i] === '\n') {
+            editorLineCount++;
+          }
+        }
+      }
+
+      if (token.type !== 'space') {
+        const index: EditorIndex = {
+          editorLineIndex,
+          editorLineCount,
+          previewElementIndex,
+        };
+
+        indices.push(index);
+        previewElementIndex++;
+      }
+      editorLineIndex += editorLineCount;
+    }
+
+    return indices;
+  }
+
+  highlightExtension() {
+    return markedHighlight({
+      emptyLangClass: 'hljs',
+      langPrefix: 'hljs-',
+      highlight: (str, lang, info) => {
         const hash = this.strHash(str + lang);
         if (this.highlightCache.has(hash)) {
           return this.highlightCache.get(hash)!;
@@ -45,16 +112,21 @@ export class MdEditorComponent {
         let rawCode = '';
         if (lang && hljs.getLanguage(lang)) {
           try {
-            rawCode = hljs.highlight(str, { language: lang, ignoreIllegals: true }).value;
+            rawCode = hljs.highlight(str, {
+              language: lang,
+              ignoreIllegals: true,
+            }).value;
           } catch (__) {
-            rawCode = this.md.utils.escapeHtml(str);
+            // TODO: escape HTML
+            rawCode = str;
           }
         } else {
-          rawCode = this.md.utils.escapeHtml(str);
+          // TODO: escape HTML
+          rawCode = str;
         }
 
         let code = rawCode;
-        if (settings.mdEditor().showLineNumbers) {
+        if (this.settings.mdEditor().showLineNumbers) {
           const split = rawCode.split('\n');
           const lineNumberDigits = split.length.toString().length;
           code = split.reduce((acc, line, i) => {
@@ -63,26 +135,18 @@ export class MdEditorComponent {
           }, '');
         }
 
-        const ret = '<pre class="hljs-code-block"><code class="hljs">' + code + '</code></pre>';
+        const ret =
+          '<pre class="hljs-code-block"><code class="hljs">' +
+          code +
+          '</code></pre>';
         this.highlightCache.set(hash, ret);
         return ret;
-      }
+      },
     });
-
-    this.editorMode = computed(() => settings.mdEditor().editorMode);
-
-    effect(() => {
-      console.time('render');
-      this.previewContent = this.md.render(this.value());
-      console.timeEnd('render');
-    });
-
-    this.onWindowResize();
   }
 
   onEditorKeydown(event: KeyboardEvent) {
     this.editorLines = null;
-    this.previewLines = null;
     if (event.key === 'Tab') {
       event.preventDefault();
       const target = event.target as HTMLTextAreaElement;
@@ -90,20 +154,13 @@ export class MdEditorComponent {
       const end = target.selectionEnd;
       const newValue =
         this.value().substring(0, start) +
-        " ".repeat(4) +
+        ' '.repeat(4) +
         this.value().substring(end);
       this.value.set(newValue);
       setTimeout(() => {
         target.selectionStart = target.selectionEnd = start + 4;
       });
     }
-  }
-
-  onEditorInput(event: Event) {
-    // TODO: Use "field-sizing: content;" in the CSS once it's supported in all browsers"
-    // const textarea = event.target as HTMLTextAreaElement;
-    // textarea.style.height = '';
-    // textarea.style.height = `${textarea.scrollHeight}px`;
   }
 
   @HostListener('window:resize')
@@ -113,37 +170,44 @@ export class MdEditorComponent {
 
   onEditorResize($event: UIEvent) {
     this.editorLines = null;
-    this.previewLines = null;
   }
 
   strHash(s: string) {
-    return s.split("").reduce(function (a, b) {
-      a = ((a << 5) - a) + b.charCodeAt(0);
+    return s.split('').reduce(function (a, b) {
+      a = (a << 5) - a + b.charCodeAt(0);
       return a & a;
     }, 0);
   }
 
-  onEditorScroll(event: Event) {
-    const target = event.target as HTMLTextAreaElement;
+  syncScrolls() {
+    const target = this.editorElementRef.nativeElement;
     const scrollOffset = target.scrollTop;
-    this.editorLines ??= this.calculateEditorLines(target);
-    const filtered = this.editorLines.filter(l => scrollOffset >= l.offset);
+    this.editorLines ??= this.calculateEditorLineSizes(target);
+    const filtered = this.editorLines.filter((l) => scrollOffset >= l.offset);
     const line = filtered[filtered.length - 1];
-    this.testDisplay = line.lineIndex.toString();
 
-    this.previewLines ??= this.calculatePreviewLines(this.previewContentElement.nativeElement);
-    const lastEl = this.previewLines[this.previewLines.length - 1];
-    const lastElLineIndex = lastEl?.lineIndex ?? 0;
-    const lastElTotalLines = lastEl?.totalLines ?? 0;
-    this.testDisplay = `Editor: ${this.value().split('\n').length}, Preview: ${lastElLineIndex + lastElTotalLines}`;
-    console.log(this.previewLines);
-    const previewLine = this.previewLines.find(l => l.lineIndex === line.lineIndex);
-    if (previewLine) {
-      previewLine.element.scrollIntoView({ block: 'start', behavior: 'smooth' });
+    let editorIndex = this.editorIndices.length - 1;
+    while (
+      editorIndex >= 0 &&
+      this.editorIndices[editorIndex].editorLineIndex > line.lineIndex
+      ) {
+      editorIndex--;
+    }
+    const editorLine = this.editorIndices[editorIndex];
+
+    const element =
+      this.previewContentElement.nativeElement.children[
+        editorLine.previewElementIndex
+        ];
+    if (element && this.syncPreview) {
+      element.scrollIntoView({
+        block: 'start',
+        behavior: 'smooth',
+      });
     }
   }
 
-  calculateEditorLines(textarea: HTMLTextAreaElement) {
+  calculateEditorLineSizes(textarea: HTMLTextAreaElement) {
     const textareaStyle = window.getComputedStyle(textarea);
 
     // Create one container for all lines
@@ -168,7 +232,7 @@ export class MdEditorComponent {
     });
 
     // Now measure each child in one pass
-    const lines: EditorLine[] = [];
+    const lines: EditorLineSize[] = [];
     let offset = 0;
     let prevOffset = 0;
     for (let i = 0; i < textLines.length; i++) {
@@ -181,7 +245,10 @@ export class MdEditorComponent {
         offset = prevOffset + prevLine.wrappedLinesCount * lineHeight;
       }
       lines.push({
-        line: textLines[i], offset, lineIndex: i, wrappedLinesCount
+        line: textLines[i],
+        offset,
+        lineIndex: i,
+        wrappedLinesCount,
       });
       prevOffset = offset;
     }
@@ -190,54 +257,10 @@ export class MdEditorComponent {
     return lines;
   }
 
-  calculatePreviewLines(element: HTMLElement) {
-    const lines: PreviewLine[] = [];
-    let totalLines = 0;
-    for (let i = 0; i < element.children.length; i++) {
-      const child = element.children[i] as HTMLElement;
-      const lineCount = this.countLines(child);
-      lines.push({
-        element: child, totalLines: lineCount, lineIndex: totalLines
-      });
-      totalLines += lineCount;
+  toggleSyncPreview() {
+    this.syncPreview = !this.syncPreview;
+    if (this.syncPreview) {
+      this.syncScrolls();
     }
-    return lines;
   }
-
-  private countLines(element: HTMLElement): number {
-    if (element instanceof HTMLUListElement || element instanceof HTMLOListElement) {
-      return element.children.length;
-    }
-
-    if (element instanceof HTMLPreElement) {
-      return this.countLines(element.children[0] as HTMLElement);
-    }
-
-    // Code block
-    if (element instanceof HTMLElement && element.tagName === 'CODE') {
-      const text = element.textContent || '';
-      let newlines = 0;
-      for (let i = 0; i < text.length; i++) {
-        if (text[i] === '\n') {
-          newlines++;
-        }
-      }
-      return newlines;
-    }
-
-    return 1;
-  }
-}
-
-interface EditorLine {
-  line: string;
-  offset: number;
-  lineIndex: number;
-  wrappedLinesCount: number;
-}
-
-interface PreviewLine {
-  element: HTMLElement;
-  totalLines: number;
-  lineIndex: number;
 }
