598addef563de523bfc96d98e66d7fee272b7d81
LukaStrbad <s.luka.1308@gmail.com>
2025-01-26 22:09:52 +0100

    Add docker compose and Dockerfiles

--- Server/AppConfiguration.cs
+++ Server/AppConfiguration.cs
@@ -8,6 +8,7 @@ public class AppConfiguration
     public string JwtPrivateKey { get; set; } = "";
     [JsonIgnore] public byte[] JwtPrivateKeyBytes => Encoding.UTF8.GetBytes(JwtPrivateKey);
 
+    [JsonIgnore]
     public string BaseDir { set; get; } = ".";
 
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
