aaf480f0461b9d8bb760df20152eaf5d132cb3e4
Luka Strbad <s.luka.1308@gmail.com>
2025-04-28 20:04:52 +0200

    Change the search endpoint from GET to POST and add SearchControllerTests

---
+++ ServerTests/ControllerTests/SearchControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Server.Controllers;
+using Server.Model;
+using Server.Services;
+
+namespace ServerTests.ControllerTests;
+
+public class SearchControllerTests
+{
+    private readonly SearchController _controller;
+    private readonly ISearchService _searchService;
+
+    public SearchControllerTests()
+    {
+        _searchService = Substitute.For<ISearchService>();
+        _controller = new SearchController(_searchService);
+    }
+
+    [Fact]
+    public async Task SearchFiles_ReturnsOk_WhenFilesSearched()
+    {
+        var searchOptions = new SearchOptionsDto("foo", 0, 100);
+        
+        var filesToReturn = new List<FileSearchResultDto>
+        {
+            new("foo", [], "foo", DateTime.Now),
+            new("foo_bar", [], "bar", DateTime.Now)
+        };
+        _searchService.SearchFiles(searchOptions).Returns(filesToReturn.ToAsyncEnumerable());
+        
+        var response = await _controller.SearchFiles(searchOptions);
+
+        var result = Assert.IsType<OkObjectResult>(response);
+        var returnedFiles = Assert.IsType<IEnumerable<FileSearchResultDto>>(result.Value, exactMatch: false);
+        
+        Assert.Equal(filesToReturn, returnedFiles);
+    }
+}
