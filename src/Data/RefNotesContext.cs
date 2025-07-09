2a741e12006250b9d1c41dc04c1e5d0f60559bd4
Luka Strbad <s.luka.1308@gmail.com>
2025-07-09 21:45:24 +0200

    Add converters for DateTime to ensure they're always in UTC

--- src/Data/RefNotesContext.cs
+++ src/Data/RefNotesContext.cs
@@ -1,6 +1,6 @@
 ﻿using Data.Model;
 using Microsoft.EntityFrameworkCore;
-using MySqlConnector;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Data;
 
@@ -27,5 +27,33 @@ public class RefNotesContext(DbContextOptions<RefNotesContext> options) : DbCont
             .UsingEntity(join => join.ToTable("encrypted_files_file_tags"));
 
         new UserGroupRoleConfiguration().Configure(modelBuilder.Entity<UserGroupRole>());
+        
+        var dateTimeConverter = new ValueConverter<DateTime, DateTime>(
+            v => v.ToUniversalTime(),
+            v => DateTime.SpecifyKind(v, DateTimeKind.Utc));
+
+        var nullableDateTimeConverter = new ValueConverter<DateTime?, DateTime?>(
+            v => v.HasValue ? v.Value.ToUniversalTime() : v,
+            v => v.HasValue ? DateTime.SpecifyKind(v.Value, DateTimeKind.Utc) : v);
+
+        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            if (entityType.IsKeyless)
+            {
+                continue;
+            }
+
+            foreach (var property in entityType.GetProperties())
+            {
+                if (property.ClrType == typeof(DateTime))
+                {
+                    property.SetValueConverter(dateTimeConverter);
+                }
+                else if (property.ClrType == typeof(DateTime?))
+                {
+                    property.SetValueConverter(nullableDateTimeConverter);
+                }
+            }
+        }
     }
 }
