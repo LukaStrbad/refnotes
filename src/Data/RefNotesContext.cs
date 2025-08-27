856ce7ff6c0dff96c7b719c41a0d3acba8878eb4
Luka Strbad <s.luka.1308@gmail.com>
2025-08-27 18:13:29 +0200

    Add SharedFileHash model

--- src/Data/RefNotesContext.cs
+++ src/Data/RefNotesContext.cs
@@ -21,6 +21,7 @@ public class RefNotesContext(DbContextOptions<RefNotesContext> options) : DbCont
     public DbSet<EmailConfirmToken> EmailConfirmTokens { get; set; }
     public DbSet<PasswordResetToken> PasswordResetTokens { get; set; }
     public DbSet<SharedFile> SharedFiles { get; set; }
+    public DbSet<SharedFileHash> SharedFileHashes { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
